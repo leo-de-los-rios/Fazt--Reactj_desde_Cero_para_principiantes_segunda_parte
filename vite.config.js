bc7e6e3881f4a09b321bcae11e0269a039e3628b
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2023-07-11 15:40:31 -0300

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Fazt--Reactj_desde_Cero_para_principiantes_segunda_parte/",
+});
